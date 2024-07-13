384b6f52e304064edbbe969ea0b657a00d21cd2a
Felipe Bernardo de Oliveira <felipebernardo55@hotmail.com>
2024-07-13 19:20:57 -0300

    add route event-id

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -92,7 +92,11 @@ class Event extends Model
 
     public function showEventId(int $id): array
     {
+        date_default_timezone_set('America/Sao_Paulo');
+        $date = new DateTime();
+        $dateString = $date->format('Y-m-d');
         return self::where('id', $id)
+            ->where('date', '>=', $dateString)
             ->with('show')
             ->with('show.atraction')
             ->with('ticket')
