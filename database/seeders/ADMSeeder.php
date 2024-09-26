f1a2de69c5d75773904be410ae967f3603eabd7b
Felipe Bernardo de Oliveira <felipebernardo55@hotmail.com>
2024-09-26 13:46:53 -0300

    wip

--- database/seeders/ADMSeeder.php
+++ database/seeders/ADMSeeder.php
@@ -12,14 +12,5 @@ class ADMSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        DB::table('profile_adms')->insert([
-            "name" => "Felipe Bernardo de Oliveira",
-            "fone" => '17991020668',
-            "cnpj" => '2318907398172',
-            "date_born" => '08-09-1998',
-            "id_user" => 1
-        ]);
-    }
+    public function run() {}
 }
