0836266636c958eebb354c8ed6f78e6ac829661f
Felipe Bernardo de Oliveira <felipebernardo55@hotmail.com>
2024-09-26 13:51:57 -0300

    wip

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,13 +12,5 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        DB::table('users')->insert([
-            'email' => 'bernardodev0809@gmail.com',
-            'password' => bcrypt(123),
-            'id_permission' => 2,
-            'auth' => 'approved'
-        ]);
-    }
+    public function run() {}
 }
